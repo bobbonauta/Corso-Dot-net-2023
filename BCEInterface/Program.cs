6713b4f46e29aa35307c17d3c5a5ffbba4b78bb5
bobbonauta <133910949+bobbonauta@users.noreply.github.com>
2023-11-10 16:07:00 +0100

    First Commit incomplete checkeuropean

---
+++ BCEInterface/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BCEInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public abstract class State
+        {
+            protected string _name;
+            protected bool _euMember;
+            protected bool _NonEuMember;
+            protected bool _DeathPenalty;
+
+            public State(string Name, bool EuMember, bool NonEuMember, bool Death)
+            {
+                _name = Name;
+                _euMember = EuMember;
+                _NonEuMember = NonEuMember;
+                _DeathPenalty = Death;
+                
+            }
+
+            public abstract double SpreadCalculate();
+
+        }
+
+        private class EuroCentralBank
+        {
+            private string _stateName { get; set; }
+            public EuroCentralBank(string StateName)
+            {
+                _stateName = StateName;
+            }
+        
+            public bool SpreadCalculate()
+            {
+                decimal TotalSpread = 1;
+                if (TotalSpread >= 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public class EuropeanHumanRights
+        {
+          public string CheckEuropeanHumanRights(State _state)
+            {
+                if (State.
+            }
+        }
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static Interfaces.Elettronic;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //mascheriamo elettronic da Iphone
+            Elettronic elettronic = new Iphone();            
+        }
+    }
+
+    interface ITelefone
+    {
+        public void Call();
+    }
+
+    interface IsmartPhone: ITelefone,ImediaPlayer
+    {
+        public string Nseriale { get; set; }
+        public void Camera();
+        public void GPS();
+    }
+
+    interface ImediaPlayer 
+    {
+        public void Play(); 
+        public void Pause();
+        public void Stop();
+    }
+
+    abstract public class Elettronic
+    {
+        public string _nSeriale;
+    }   
+     public class Iphone:Elettronic, IsmartPhone
+            
+     {
+            public string Nseriale { get; set; }
+
+            public void Call() {}
+            public void Play() {}
+            public void Pause() {}
+            public void Stop() {}
+            public void Camera() { }
+            public void GPS() { }
+     }
+        
+        
+}
+
