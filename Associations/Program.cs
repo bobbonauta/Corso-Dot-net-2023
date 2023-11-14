814b287bb96e51400e7187a5d6268be618d5f333
bobbonauta <133910949+bobbonauta@users.noreply.github.com>
2023-11-14 14:45:14 +0100

    Associazioni first up - incomplete

---
+++ Associations/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace StateAssociation
+{
+    internal class Program
+    {
+        // Interfaccia per le entità amministrative
+        public interface PublicEntity
+        {
+            string Flag { get; }
+            string Money { get; }
+            decimal Cost { get; }
+            string BorderLimits { get; }
+        }
+
+        // Classe di base per le entità amministrative
+        public abstract class State : PublicEntity
+        {
+            protected string _flag;
+            protected string _money;
+            protected string _army;
+            protected decimal _cost;
+            protected string _borderLimits;
+
+            public State(string flag, string money, string army, decimal cost, string borderLimits)
+            {
+                _flag = flag;
+                _money = money;
+                _army = army;
+                _cost = cost;
+                _borderLimits = borderLimits;
+            }
+
+            public string Flag { get { return _flag; } }
+            public string Money { get { return _money; } }
+            public decimal Cost { get { return _cost; } }
+            public string BorderLimits { get { return _borderLimits; } }
+        }
+
+        // Classe per la Regione
+        public class Region : State
+        {
+            
+            public Region(string flag, string money, string army, decimal cost, string borderLimits)
+                : base(flag, money, army, cost, borderLimits)
+            {
+                
+            }
+        }
+
+        // Classe per la Provincia
+        public class Province : State
+        {
+            
+            public Province(string flag, string money, string army, decimal cost, string borderLimits)
+                : base(flag, money, army, cost, borderLimits)
+            {
+               
+            }
+        }
+
+        // Classe per il Comune
+        public class Municipality : State
+        {
+            
+            public Municipality(string flag, string money, string army, decimal cost, string borderLimits)
+                : base(flag, money, army, cost, borderLimits)
+            {
+                
+            }
+        }
+
+        // Classe per il Cittadino
+        public class Cittadino
+        {
+            
+            private Municipality _comune;
+
+            public Cittadino(Municipality comune)
+            {
+                _comune = comune;
+            }
+
+            
+        }
+    }
+}
